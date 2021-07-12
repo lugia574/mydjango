ebb381947607bbf36252cba8eb15c810440ca3be
lcw57 <dlcjfdnr789@naver.com>
2021-07-12 17:20:25 +0900

    라우팅, DB 출력 21/07/12

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
+
+
 
 
 def hello_world (request):
     # 라우팅 해줘야해 (연결)
     # 메인 앱에서 먼저 받고 여기로 보냄
-
-
-
 
     if request.method == 'POST': # 기본적으로 겟 방식으로 작동함
         temp = request.POST.get('input')
@@ -18,7 +19,11 @@ def hello_world (request):
         new_data.text = temp
         new_data.save()
 
-
-        return render(request, 'account/hello_world.html', context={'text': 'POST METHOD!'})
+        # data_list = HelloWorld.objects.all()
+        # return render(request, 'account/hello_world.html', context={'data_list': data_list})
+        
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'account/hello_world.html', context={'text': 'GET METHOD!'})
+
+        data_list = HelloWorld.objects.all()
+        return render(request, 'account/hello_world.html', context={'data_list': data_list})
