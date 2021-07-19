cf2f006acb9a2ffda41de654b0058721dd2a2433
lcw57 <dlcjfdnr789@naver.com>
2021-07-19 17:09:32 +0900

    21/07/19 - 1 update 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import HelloWorld
 
@@ -41,3 +41,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name ='target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:update.html')
+    template_name = 'accountapp/update.html'
